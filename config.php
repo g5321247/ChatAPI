1e67a6cf1af590b89a5f21c8eb1879f268205291
GeorgeLIu <g5321247@gmail.com>
2019-11-30 23:54:51 +0800

    [Update] 將 PDO 抓的檔案改為由 config 給值

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
   'database' => [
-    'name' => 'Line',
+    'name' => 'mytodo',
     'username' => 'root',
     'password' => '1234',
     'connection' => 'mysql:host=127.0.0.1',
--- core/database/Connection.php
+++ core/database/Connection.php
@@ -4,7 +4,8 @@ class Connection {
 
   public static function make($config) {
     try {
-      return new PDO('mysql:host=127.0.0.1;dbname=Line', 'root', "1234");
+      // return new PDO('mysql:host=127.0.0.1;dbname=', 'root', "1234");
+      return new PDO("$config[connection];dbname= $config[name]", "$config[username]", "$config[password]");
     } catch (\Exception $e) {
         die($e->getmessage());
     }
