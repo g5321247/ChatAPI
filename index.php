f44311459c62a46e9a97b335f3d7faa2cc88f6a0
GeorgeLIu <g5321247@gmail.com>
2019-12-09 22:28:44 +0800

    [Update] delete content in index

--- index.php
+++ index.php
@@ -3,28 +3,6 @@
 require 'object/user.php';
 
 $queryBuilder = require 'bootstrap.php';
-$users =  $queryBuilder -> selectAll('User');
-
-if (count($users)>0) {
-  $userArray = array();
-  $userArray["users"] = array();
-
-  foreach ($users as $key => $userModel) {
-    $user = array(
-      "id" => $userModel->uuid,
-      "name" => $userModel->name,
-      "photo" => $userModel->photo,
-    );
-
-    array_push($userArray["users"], $user);
-  }
-
-  http_response_code(200);
-  echo json_encode($userArray);
-}
-
-
-// var_dump($users);
 
 ?>
 
