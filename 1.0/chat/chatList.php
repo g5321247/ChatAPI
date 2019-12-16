81000344c05c3c0821dd7a0933407f79d6b33381
GeorgeLIu <g5321247@gmail.com>
2019-12-16 10:40:30 +0800

    [Refactor] 將 chatList 物件化

--- 1.0/chat/chatList.php
+++ 1.0/chat/chatList.php
@@ -9,9 +9,8 @@ include(PATH."/error.php");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-global $queryBuilder;
-$queryBuilder = require (dirname(dirname(dirname(__FILE__)))."/bootstrap.php");
 
+$queryBuilder = require (dirname(dirname(dirname(__FILE__)))."/bootstrap.php");
 $userID = isset($_GET['userID']) ? $_GET['userID']:'';
 
 if (empty($userID)) {
@@ -19,65 +18,78 @@ if (empty($userID)) {
   exit;
 }
 
-function getUser($id) {
-  $sql = "select * from user WHERE id = '{$userID}'";
-  $users =  $queryBuilder -> queryProperty($sql,'user');
-  return $users[0];
-}
+$chatList = new ChatList($queryBuilder,$userID);
+$chatList->getChatList();
 
+class ChatList {
 
-$sql = "select * from user WHERE id = '{$userID}'";
-$users =  $queryBuilder -> queryProperty($sql,'user');
+  protected $queryBuilder;
+  protected $userID;
 
-if (empty($users[0])) {
-  ErrorMessage::getMessage("invalid user id");
-
-  exit;
-}
-
-$chatList = $users[0] -> getResult();
-
-$sql = "select * from group_user WHERE userID = '{$userID}'";
-$groupList = $queryBuilder -> queryProperty($sql,'groupList');
-
-// 群組列表
-$groupsID = array();
-foreach ($groupList as $list) {
-  $groupsID[] = ($list->groupID);
-}
-
-$groupsID = implode(',',$groupsID);
-$groupSql = "select * from groups WHERE id IN ({$groupsID})";
-$groups =  $queryBuilder -> queryProperty($groupSql,'group');
-
-//排序規則
-function cmp($a, $b) {
-    return $a->messageTime < $b->messageTime;
-}
-
-usort($groups, "cmp");
-
-$groupsList = array();
-
-foreach ($groups as $group) {
-  // 0 為單人
-  if ($group->type == 0) {
-    $groupSql = "select * from group_user WHERE groupID IN ({$group->id}) AND userID <> ({$userID})";
-    $list =  $queryBuilder -> queryProperty($groupSql,'groupList');
-    $friendID = $list[0]->userID;
-    $sql = "select * from user WHERE id = '{$friendID}'";
-    $users =  $queryBuilder -> queryProperty($sql,'user');
-    $group->title = $users[0]->name;
-    $group->image = $users[0]->photo;
+  function __construct($queryBuilder, $userID) {
+    $this->queryBuilder = $queryBuilder;
+    $this->userID = $userID;
   }
 
-  $result = $group->getChatListResult();
-  $groupsList[] = $result;
-}
+  function getChatList() {
+    $user = $this->getUser();
 
-echo json_encode(
-  [
-    "chat_lists" => $groupsList
-  ]
-);
+    if (empty($user)) {
+      ErrorMessage::getMessage("invalid user id");
+      exit;
+    }
+
+    $groups = $this->getGroupsList();
+
+    echo json_encode(
+      [
+        "chat_lists" => $groups
+      ]
+    );
+  }
+
+  protected function getUser() {
+    $sql = "select * from user WHERE id = '{$this->userID}'";
+    $users =  $this->queryBuilder -> queryProperty($sql,'user');
+    return $users[0];
+  }
+
+  function getGroupsList() {
+    $groups = $this->getGroups();
+    $groupsList = array();
+
+    foreach ($groups as $group) {
+      // 0 為單人
+      if ($group->type == 0) {
+        $groupSql = "select * from group_user WHERE groupID IN ({$group->id}) AND userID <> ({$this->userID})";
+        $list =  $this->queryBuilder -> queryProperty($groupSql,'groupList');
+        $friendID = $list[0]->userID;
+        $sql = "select * from user WHERE id = '{$friendID}'";
+        $users =  $this->queryBuilder -> queryProperty($sql,'user');
+        $group->title = $users[0]->name;
+        $group->image = $users[0]->photo;
+      }
+
+      $result = $group->getChatListResult();
+      $groupsList[] = $result;
+    }
+    return $groupsList;
+  }
+
+  protected function getGroups() {
+    $sql = "select * from group_user WHERE userID = '{$this->userID}'";
+    $groupList = $this->queryBuilder -> queryProperty($sql,'groupList');
+
+    $groupsID = array();
+    foreach ($groupList as $list) {
+      $groupsID[] = ($list->groupID);
+    }
+
+    $groupsID = implode(',',$groupsID);
+    $groupSql = "select * from groups WHERE id IN ({$groupsID}) ORDER BY messageTime DESC";
+    $groups =  $this->queryBuilder -> queryProperty($groupSql,'group');
+
+    return $groups;
+  }
+}
 ?>
